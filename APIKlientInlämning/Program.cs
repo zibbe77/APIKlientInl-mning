d98721d2753133313b4d4b4c0db3b791dc7c39da
Theo zibell <theo@zibell.se>
2022-11-16 12:22:53 +0100

    first

---
+++ APIKlientInlämning/Program.cs
@@ -0,0 +1,6 @@
+﻿using RestSharp;
+using System.Text.Json;
+
+RestClient pokeClient = new RestClient("https://pokeapi.co/api/v2/");
+RestRequest request = new RestRequest("pokemon/charmander");
+RestResponse response = pokeClient.GetAsync(request).Result;
